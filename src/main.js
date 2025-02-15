ee2b224de62f8b9906cf0e4e8f3e1d9506382483
Pedro Queiroz <douglas.333.goncalves@gmail.com>
2025-02-14 23:34:40 -0300

    incluindo o css e o html com webpach

--- src/main.js
+++ src/main.js
@@ -1,2 +1,10 @@
+"use strict";
+
+/* importando os modulos css */
+import "./styles/global.css";
+import "./styles/input.css";
+import "./styles/modal.css";
+import "./styles/period.css";
+
 import "./modules/modal/modal-open-and-close.js";
 import "./modules/date-change.js";
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  target: "web",
+  mode: "development",
+
+  entry: path.resolve(__dirname, "src", "main.js"),
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  //CONFIGURAÇÕES DO WEBPACK DEVSERVER//
+  devServer: {
+    static: { directory: path.join(__dirname, "dist") },
+    port: 3000,
+    open: true,
+    liveReload: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "index.html"),
+    }),
+  ],
+
+  module: {
+   rules: [
+      {
+        test: /\.css$/, //Essa é uma regra para pegar todos os arquivos .css
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
